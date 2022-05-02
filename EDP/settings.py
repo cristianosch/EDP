4d92b39896ef5f3c00338c59773eb565573b27a4
Cristiano Schroeder <84641175+cristianosch@users.noreply.github.com>
2022-05-03 00:06:13 +0100

    Update settings.py

--- EDP/settings.py
+++ EDP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rhq87envgxsl+3nx&!+7yqr@go-&4vjp#(j%l-q9+$^&@(!)fx'
+# SECRET_KEY = 'django-insecure-rhq87envgxsl+3nx&!+7yqr@go-&4vjp#(j%l-q9+$^&@(!)fx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
